8e6eb15b1797cee577a5334152a50bee12d13728
mdlawrenceusa <mdlawrence@me.com>
2020-07-14 11:46:00 -0400

    1 file modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -11,7 +11,7 @@ export class ProductListComponent {
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert('The product has been shared with College Now!');
   }
 }
 
