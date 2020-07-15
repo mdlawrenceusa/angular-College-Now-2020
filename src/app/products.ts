42e52642e5202558f1a6313327034dcf2ae1efc1
mdlawrenceusa <mdlawrence@me.com>
2020-07-15 11:52:44 -0400

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,7 +2,8 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    praise: 'College Now Students are Great'
   },
   {
     name: 'Phone Mini',
